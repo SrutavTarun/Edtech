03f757d599628f43b85f2761633d72769534998e
Srutav Tarun <srutavtarun@gmail.com>
2023-10-20 01:50:30 +0530

    added temp navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import "./styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">LOGO</div>
+      <ul>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">ABOUT US</a>
+        </li>
+        <li>
+          <a href="#">SERVICES</a>
+        </li>
+        <li className="contact">
+          <a href="#">CONTACT US</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
