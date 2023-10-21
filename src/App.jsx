a0c4cacd6df32b01555f068f36142b02c467ccae
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-10-22 00:51:08 +0530

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import Navbar from './components/navbar'
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import About from "./components/about";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
+      <Home />
+      <About />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,4 @@
+const home = () => {
+  return <div className="home"></div>;
+};
+export default home;
