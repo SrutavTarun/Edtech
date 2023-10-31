fc23096543d0dd8c58d3714421815c4c75535911
Nahal Roshan K <nahalroshan5@gmail.com>
2023-10-31 19:03:53 +0530

    sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import React from 'react';
+import './App.css';
+//import Navbar from './components/navbar';
+import WhatCanYouDoSection from './components/whatCanYouSection'; // Correct import statement
 import './App.css'
-
+import HowToUseUsSection from './components/howToUseUsSection';
+import './components/howToUseUsSection'
 import Navbar from './components/navbar'
-
+import './components/whatCanYouSection'
 function App() {
-  const [count, setCount] = useState(0)
+ // const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
+  <WhatCanYouDoSection />
+  <HowToUseUsSection />
+    
     </>
   )
 }
---
+++ src/components/howToUseUsSection.jsx
@@ -0,0 +1,67 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './styles/howToUseUsSection.css'
+function HowToUseUsSection() {
+  return (
+    <div>
+        <div>
+          
+            <h1 className="underlined-text">How to Use Us ?</h1>
+            <div className='flex-card'>
+            <div className='card-container'>
+                <div className='image-section'>
+                   <h2 className='header'>1</h2>
+                </div>
+                <h1>Browse Courses</h1>
+                <p>Start by browsing our catalogue of courses. You can filter by subject,
+level, and popularity to find the courses that interest you.
+</p>
+            </div>
+            <div className='card-container'>
+                <div className='image-section'>
+                <h2 className='header'>2</h2>
+                </div>
+                <h1>Sign Up </h1>
+                <p>To access courses and all the features, create an account. You can sign up as a
+student, creator</p>
+            </div>
+            <div className='card-container'>
+                <div className='image-section'>
+                <h2 className='header'>3</h2>
+                </div>
+                <h1>Enrol</h1>
+                <p>Once you've found a course you like, enrol in it. You'll gain access to the course
+content and can begin your learning journey</p>
+            </div>
+            <div className='card-container'>
+                <div className='image-section'>
+                <h2 className='header'>4</h2>
+                </div>
+                <h1>Interact</h1>
+                <p>Engage with the community. Ask questions, participate in discussions, and
+connect with fellow learners and educators.</p>
+            </div>
+            <div className='card-container'>
+                <div className='image-section'>
+                <h2 className='header'>5</h2>
+                </div>
+                <h1>Teach or Create</h1>
+                <p>If you're a creator, use our easy course creation tools to build your
+course. Publish it and start sharing your knowledge.</p>
+            </div>
+            <div className='card-container'>
+                <div className='image-section'>
+                <h2 className='header'>6</h2>
+                </div>
+                <h1>Payment</h1>
+                <p>For educators, set a price for your course, and students can purchase it.</p>
+            </div>
+            </div>
+           
+                
+        </div>
+    </div>
+  )
+}
+
+export default HowToUseUsSection
