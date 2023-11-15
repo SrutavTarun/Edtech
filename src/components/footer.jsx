4ee2699bd4b3cc185b0f9cabc8a4563ae70e3a7a
Srutav Tarun <srutavtarun@gmail.com>
2023-11-15 20:12:46 +0530

    fixed mobile responsiveness

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,74 +1,87 @@
 import { Link } from "react-scroll";
 
 import "./styles/footer.css";
+import logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="ftop">
         <div className="fleft">
-          <div className="flogo">LOGO</div>
+          <div className="flogo"><img src={logo} alt="" className="footer-logo-img"/></div>
           {/* <div className="fltext">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor Lorem ipsum dolor sit amet,
           </div> */}
         </div>
-        <div className="fmid">
-          <div className="fmhead">Useful Links</div>
-          <div className="flist">
-            <Link
-              to="home"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className="flist-item"
-            >
-              Home
-            </Link>
-            <Link
-              to="about"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className="flist-item"
-            >
-              About Us
-            </Link>
-            <Link
-              to="services"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className="flist-item"
-            >
-              Services
-            </Link>
-            <Link
-              to="contact"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className="flist-item"
-            >
-              Contact
-            </Link>
+        <div className="fleft-mid">
+          <div className="fmid">
+            <div className="fmhead">Useful Links</div>
+            <div className="flist">
+              <div className="f-mid-row1">
+                <Link
+                  to="home"
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  className="flist-item"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="about"
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  className="flist-item"
+                >
+                  About Us
+                </Link>
+              </div>
+              <div className="f-mid-row2">
+                <Link
+                  to="services"
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  className="flist-item"
+                >
+                  Services
+                </Link>
+                <Link
+                  to="contact"
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  className="flist-item"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="fright">
-          <div className="frhead">Follow us</div>
-          <div className="flinks">
-            <div className="flink-item">Facebook</div>
-            <div className="flink-item">Instagram</div>
-            <div className="flink-item">Twitter</div>
-            <div className="flink-item">Youtube</div>
+          <div className="fright">
+            <div className="frhead">Follow us</div>
+            <div className="flinks">
+              <div className="f-right-row1">
+                <div className="flink-item">Facebook</div>
+                <div className="flink-item">Instagram</div>
+              </div>
+              <div className="f-right-row2">
+                <div className="flink-item">Twitter</div>
+                <div className="flink-item">Youtube</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div className="fline"></div>
       <div className="fbottom">
-        <div className="fcopyright">&copy; Copyright Logo</div>
-        <div className="fprivacy">Privacy Policy</div>
-        <div className="fterms">Terms &#38; Conditions</div>
+        <div className="fcopyright">Copyright &copy; 2023 Knowlumi</div>
+        <div className="f-bottom-row2">
+          <div className="fprivacy">Privacy Policy</div>
+          <div className="fterms">Terms &#38; Conditions</div>
+        </div>
       </div>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,39 +1,53 @@
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 import "./styles/navbar.css";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
   return (
-    <div className="nav-navbar">
-      <div className="nav-logo">LOGO</div>
-      <ul>
-        <li>
-          <Link to="home" smooth={true} offset={-70} duration={500}>
-            HOME
-          </Link>
-        </li>
-        <li>
-          <Link to="about" smooth={true} offset={-70} duration={500}>
-            ABOUT US
-          </Link>
-        </li>
-        <li>
-          <Link to="services" smooth={true} offset={-70} duration={500}>
-            SERVICES
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="contact"
-            smooth={true}
-            offset={-70}
-            duration={500}
-            className="nav-contact"
-          >
-            CONTACT US
-          </Link>
-        </li>
-      </ul>
+    <div className={`nav-navbar ${mobileMenu ? 'active' : ''}`}>
+      <div className="nav-logo"><img src={logo} alt="" className="nav-logo-img"/></div>
+      <div>
+        <ul>
+          <li>
+            <Link to="home" smooth={true} offset={-70} duration={500}>
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} offset={-70} duration={500}>
+              ABOUT US
+            </Link>
+          </li>
+          <li>
+            <Link to="services" smooth={true} offset={-70} duration={500}>
+              SERVICES
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              className="nav-contact"
+            >
+              CONTACT US
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile" onClick={toggleMobileMenu}>
+        {mobileMenu ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+      </div>
     </div>
   );
 };
